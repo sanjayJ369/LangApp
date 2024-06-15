4f230c2d1a9f37def0f1abec83e2b0d8df168cdc
cosmos-369 <cosmos333999@gmail.com>
2024-06-15 20:33:21 +0530

    add flashchards test

---
+++ feature/flashcards_test.go
@@ -0,0 +1,18 @@
+package feature_tests
+
+import (
+	"testing"
+)
+
+func TestFlashcards(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Learner creates flashcards", func(t *testing.T) {
+		t.Parallel()
+
+		// When the Learner pass some text.
+
+		// Then the Learner receives flashcards from it.
+
+	})
+}
