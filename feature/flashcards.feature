4b64df4ce3ae4eb74834405474fe28b7496a743c
raspberry <you@example.com>
2024-06-21 22:43:19 +0400

    added more scenarious for the flashcards

--- feature/flashcards.feature
+++ feature/flashcards.feature
@@ -5,5 +5,20 @@ Feature: Flashcards
     Then the Learner receives flashcards from it
 
   Scenario: Learner gets flashcards without duplicates
-   When the Learner passes some text with repeated words
-   Then the Learner receives flashcards without duplicates
+    When the Learner passes some text with repeated words
+    Then the Learner receives flashcards without duplicates
+
+  Scenario: Learner does not get new flashcards from the same text
+    When the Learner passes some text
+    Then the Learner receives flashcards from it
+    When the Learner passes the same text again
+    Then the Learner does not receive new flashcards
+
+  Scenario Outline: Learner can learn flashcards
+    When the Learner receives a flashcard
+    Then the Learner can <guess> the meaning of it
+    And the flashcard becomes <memorized>
+    Examples:
+      | <guess> | <memorized> |
+      | right   | yes         |
+      | wrong   | no          |
--- feature/flashcards_test.go
+++ feature/flashcards_test.go
@@ -24,4 +24,47 @@ func TestFlashcards(t *testing.T) {
 		// Then the Learner receives flashcards without duplicates.
 
 	})
+
+	t.Run("Learner does not get new flashcards from the same text", func(t *testing.T) {
+		t.Parallel()
+
+		// When the Learner passes some text.
+
+		// Then the Learner receives flashcards from it.
+
+		// When the Learner passes the same text again.
+
+		// Then the Learner does not receive new flashcards.
+
+	})
+
+	t.Run("Learner can learn flashcards", func(t *testing.T) {
+		t.Parallel()
+
+		type testCase struct {
+			Guess     string `field:"<guess>"`
+			Memorized string `field:"<memorized>"`
+		}
+
+		testCases := map[string]testCase{
+			"right_yes": {"right", "yes"},
+			"wrong_no":  {"wrong", "no"},
+		}
+
+		for name, testCase := range testCases {
+			testCase := testCase
+
+			t.Run(name, func(t *testing.T) {
+				t.Parallel()
+
+				_ = testCase // TODO: Use and remove.
+				// When the Learner receives a flashcard.
+
+				// Then the Learner can <guess> the meaning of it.
+
+				// And the flashcard becomes <memorized>.
+
+			})
+		}
+	})
 }
